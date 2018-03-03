d52346629f5a4aceec6e44b2dd8fb96c043fe18f
Shrish Chaturvedi <syrus989@gmail.com>
2018-03-02 22:50:46 -0600

    workign solution

---
+++ assets/javascript/game.js
@@ -0,0 +1,122 @@
+// Wins
+var win = 0;
+
+// Current Word for computer
+var currentWord = "";
+
+// current word for user display
+var currentWordDisplay = "";
+
+// user guess container
+var userGuessArray = [];
+
+// Number of guesses remaining
+var guess = 10;
+
+// Word object
+var hangmanWord = {
+    w1: ["h", "a", "r", "r", "y"],
+    w2: ["d", "u", "m", "b", "l", "e", "d", "o", "r", "e"],
+    w3: ["g", "r", "i", "f", "f", "i", "n", "d", "o", "r"],
+    w4: ["h", "e", "r", "m", "o", "i", "n", "e"],
+    w5: ["r", "a", "v", "e", "n", "c", "l", "a", "w"],
+    w6: ["v", "o", "l", "d", "e", "m", "o", "r", "t"],
+    w7: ["h", "o", "g", "w", "a", "r", "t", "s"],
+    w8: ["q", "u", "i", "d", "d", "i", "t", "c", "h"],
+    w9: ["h", "e", "d", "w", "i", "g"],
+    w10: ["m", "u", "g", "g", "l", "e"]
+
+};
+
+
+// function to print letters guessed by user so far
+
+function printUserGuess(userguess) {
+    var printWord = "";
+    for (i = 0; i < userguess.length; i++) {
+        printWord = printWord + userguess[i] + " ";
+    };
+
+    return printWord;
+};
+
+
+// function to print status of current word
+
+function printUnderscore(userguess) {
+    var printWord = "";
+    for (i = 0; i < currentWord.length; i++) {
+
+        if (userguess.indexOf(currentWord[i]) > -1) {
+            printWord = printWord + currentWord[i] + " ";
+        }
+        else {
+            printWord = printWord + "_ ";
+        };
+    };
+
+    return printWord;
+};
+
+
+// function  to initialize the game to a starting point
+
+function initGame() {
+    guess = 10;
+    currentWord = hangmanWord["w" + Math.floor(Math.random() * 10)];
+    userGuessArray.length = 0;
+    currentWordDisplay = printUnderscore(userGuessArray);
+    console.log(currentWord);
+    document.getElementById("win-count").innerHTML = win;
+    document.getElementById("current-word").innerHTML = currentWordDisplay;
+    document.getElementById("guess-remaining").innerHTML = guess;
+    document.getElementById("letters-guessed").innerHTML = printUserGuess(userGuessArray);
+}
+
+
+// ----------------------------------------------------------------------------------------------
+// Start of the game
+// ----------------------------------------------------------------------------------------------
+
+// Set initial status
+initGame();
+
+
+document.onkeyup = function (event) {
+
+    if (guess > 0) {
+        var currentKey = event.key;
+
+        if (!userGuessArray.includes(currentKey)) {
+
+            // display the letters currently guessed by the user
+            userGuessArray.push(currentKey);
+            document.getElementById("letters-guessed").innerHTML = printUserGuess(userGuessArray);
+
+            // check if the current user key is among the letters in the chosen word
+            // display letter in 'current word' if it matches
+            currentWordDisplay = printUnderscore(userGuessArray);
+            document.getElementById("current-word").innerHTML = currentWordDisplay;
+
+            // decrement 'guess' and display it's updated value
+            guess--;
+            document.getElementById("guess-remaining").innerHTML = guess;
+
+            // check if the letter's been correctly gussed and start a new game
+            if (guess > -1 && !currentWordDisplay.includes("_")) {
+                win++;
+                document.getElementById("win-count").innerHTML = win;
+                initGame();
+            }
+            // check if the guess limit is crossed and start a new game
+            else if (guess == 0) {
+                initGame();
+            };
+        }
+    };
+
+
+};
+
+
+
